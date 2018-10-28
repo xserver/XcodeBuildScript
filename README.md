8c9229d46d1949a9f7b22faeeb98e3e19a460665
XServer <boolsql@gmail.com>
2018-10-28 09:50:12 +0800

    personal

--- README.md
+++ README.md
@@ -1,23 +1,25 @@
-## iOS自动打包
+## iOS 自动打包
 
 - Fastlane
 - Jenkins
-- xcodebuild
+- Python
+- Ruby
+- Shell
+
+xcodebuild “最后的劳动者”
 
 ## 概念
 
 workspace(.xcworkspace)可包含多个 projet，并管理它们的相互引用；
 
-project(.xcodeproj) 可包含了多个targets，是代码文件、资源、配置信息的仓库；
+project(.xcodeproj) 可包含多个 targets，是代码文件、资源、配置信息的仓库；
 
-targets 配置对文件进行编译（编译那几个文件，使用什么编译脚本）
-configuration 可以对project和targets中
+targets 配置，给编译系统提供输入（编译那几个文件，使用什么编译脚本）
 
-scheme（方案）它不同于上面四个，表示一个组合方案表示那些targets使用什么 build configuration与 executable configuration 。
-An Xcode scheme defines a collection of targets to build, a configuration to use when building, and a collection of tests to execute.可以针对编译，运行，单元测试，动态分析，静态代码分析以及打包进行一些配置
+scheme 配置 targets 使用什么 build configuration 与 executable configuration。[Build, Run, Test, Profile, Analyze, Archive]编译，运行，单元测试，动态分析，静态代码分析以及打包
 
 
-## python
+## python 3
 1、将 autobuild.py 、exportOptions.plist文件放到你的项目根目录下（即与xx.xcworkspace或者xx.xcworkspace在同一个目录下）  
 2、修改配置：autobuild.py
 3、运行脚本：autobuild.py
--- autobuild.py
+++ autobuild.py
@@ -8,100 +8,40 @@ import argparse
 import subprocess
 import requests
 import os
+import sys
 import datetime
 
-#configuration for iOS build setting
-CONFIGURATION = "Release"
-EXPORT_OPTIONS_PLIST = "exportOptions.plist"
+# input
+TARGET = 'choose'
+CONFIGURATION = "Debug"	# Release|Debug
+PLIST_PATH = "/Users/.../choose-Info.plist"
+EXPORT_OPTIONS_PLIST = "./exportOptions.plist"
+VERSION = '1.2'
+BUILD = '0'	# when BUILD = 0, BUILD = plist.build
 
-#发布版本号
-VERSION = '1.0.0'
-BUILD = '17021803'
-
-#要打包的TARGET名字
-TARGET = 'ULife'
-
-#Info.plist路径
-PLIST_PATH = "xxxxxx/Info.plist"
-
-#存放路径以时间命令
-DATE = datetime.datetime.now().strftime('%Y-%m-%d_%H.%M.%S')
-
-#会在桌面创建输出ipa文件的目录
-EXPORT_MAIN_DIRECTORY = "~/Desktop/" + TARGET + DATE
-
-#xcarchive文件路径（含有dsym），后续查找BUG用途
-ARCHIVEPATH = EXPORT_MAIN_DIRECTORY + "/%s%s.xcarchive" %(TARGET,VERSION)
-
-#ipa路径
-IPAPATH = EXPORT_MAIN_DIRECTORY + "/%s.ipa" %(TARGET)
-
-#苹果开发者账号
+# input option (Apple Store)
 APPLEID = 'xxxxxx'
 APPLEPWD = 'xxxxx'
 
-# configuration for pgyer
+# output
+EXPORT_MAIN_DIRECTORY = "~/Desktop/" + TARGET + '_' + datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+ARCHIVEPATH = EXPORT_MAIN_DIRECTORY + "/%s%s.xcarchive" %(TARGET,VERSION)
+IPAPATH = EXPORT_MAIN_DIRECTORY + "/%s.ipa" %(TARGET)
+
+
+# 蒲公英 configuration
 PGYER_UPLOAD_URL = "http://www.pgyer.com/apiv1/app/upload"
 DOWNLOAD_BASE_URL = "http://www.pgyer.com"
 USER_KEY = "xxxxxx"
 API_KEY = "xxxxx"
-#设置从蒲公英下载应用时的密码
-PYGER_PASSWORD = "xxxxx"
+PYGER_PASSWORD = "xxxxx"	#设置从蒲公英下载应用时的密码
 
-def cleanArchiveFile():
-	cleanCmd = "rm -r %s" %(ARCHIVEPATH)
-	process = subprocess.Popen(cleanCmd, shell = True)
-	process.wait()
-	print "cleaned archiveFile: %s" %(ARCHIVEPATH)
 
-def uploadIpaToAppStore():
-	print "iPA上传中...."
-	altoolPath = "/Applications/Xcode.app/Contents/Applications/Application\ Loader.app/Contents/Frameworks/ITunesSoftwareService.framework/Versions/A/Support/altool"
+def printConfiguration():
+	print(EXPORT_MAIN_DIRECTORY)
+	print('xcarchive\t:' + ARCHIVEPATH)
+	print('ipa\t:' + IPAPATH)
 
-	exportCmd = "%s --validate-app -f %s -u %s -p %s -t ios --output-format xml" % (altoolPath, IPAPATH, APPLEID,APPLEPWD)
-	process = subprocess.Popen(exportCmd, shell=True)
-	(stdoutdata, stderrdata) = process.communicate()
-
-	validateResult = process.returncode
-	if validateResult == 0:
-		print '~~~~~~~~~~~~~~~~iPA验证通过~~~~~~~~~~~~~~~~'
-		exportCmd = "%s --upload-app -f %s -u %s -p %s -t ios --output-format normal" % (
-		altoolPath, IPAPATH, APPLEID, APPLEPWD)
-		process = subprocess.Popen(exportCmd, shell=True)
-		(stdoutdata, stderrdata) = process.communicate()
-
-		uploadresult = process.returncode
-		if uploadresult == 0:
-			print '~~~~~~~~~~~~~~~~iPA上传成功'
-		else:
-			print '~~~~~~~~~~~~~~~~iPA上传失败'
-	else:
-		print "~~~~~~~~~~~~~~~~iPA验证失败~~~~~~~~~~~~~~~~"
-
-def parserUploadResult(jsonResult):
-	resultCode = jsonResult['code']
-	if resultCode == 0:
-		downUrl = DOWNLOAD_BASE_URL +"/"+jsonResult['data']['appShortcutUrl']
-		print "Upload Success"
-		print "DownUrl is:" + downUrl
-	else:
-		print "Upload Fail!"
-		print "Reason:"+jsonResult['message']
-
-def uploadIpaToPgyer(ipaPath):
-	print "ipaPath:"+ipaPath
-	ipaPath = os.path.expanduser(ipaPath)
-	ipaPath = unicode(ipaPath, "utf-8")
-	files = {'file': open(ipaPath, 'rb')}
-	headers = {'enctype':'multipart/form-data'}
-	payload = {'uKey':USER_KEY,'_api_key':API_KEY,'publishRange':'2','isPublishToPublic':'2', 'password':PYGER_PASSWORD}
-	print "uploading...."
-	r = requests.post(PGYER_UPLOAD_URL, data = payload ,files=files,headers=headers)
-	if r.status_code == requests.codes.ok:
-		result = r.json()
-		parserUploadResult(result)
-	else:
-		print 'HTTPError,Code:'+r.status_code
 
 def exportArchive():
 	exportCmd = "xcodebuild -exportArchive -archivePath %s -exportPath %s -exportOptionsPlist %s" %(ARCHIVEPATH, EXPORT_MAIN_DIRECTORY, EXPORT_OPTIONS_PLIST)
@@ -110,8 +50,8 @@ def exportArchive():
 
 	signReturnCode = process.returncode
 	if signReturnCode != 0:
-		print "export %s failed" %(TARGET)
-		return ""
+		print ("export %s failed" %(TARGET))
+		return "export faild"
 	else:
 		return EXPORT_MAIN_DIRECTORY
 
@@ -119,10 +59,9 @@ def buildProject(project):
     archiveCmd = 'xcodebuild -project %s -scheme %s -configuration %s archive -archivePath %s -destination generic/platform=iOS' %(project, TARGET, CONFIGURATION, ARCHIVEPATH)
     process = subprocess.Popen(archiveCmd, shell=True)
     process.wait()
-
     archiveReturnCode = process.returncode
     if archiveReturnCode != 0:
-        print "archive project %s failed" %(project)
+        print ("archive project %s failed" %(project)) 
         cleanArchiveFile()
 
 def buildWorkspace(workspace):
@@ -132,58 +71,145 @@ def buildWorkspace(workspace):
 
 	archiveReturnCode = process.returncode
 	if archiveReturnCode != 0:
-		print "archive workspace %s failed" %(workspace)
+		print ("archive workspace %s failed" %(workspace))
 		cleanArchiveFile()
 
+
+def upload2Pgyer():
+	print ("~~~~~~~~~~~~~~~~是否上传到蒲公英~~~~~~~~~~~~~~~~")
+	print ("        1 不上传 (默认)")
+	print ("        2 上传 ")
+	isuploadpgyer = raw_input("您的决定：")
+	# if isuploadpgyer == "2" and exportarchive != "":
+	if isuploadpgyer == '2':
+		uploadIpaToPgyer(IPAPATH)
+
+def uploadIpaToPgyer(ipaPath):
+	print ("ipaPath:"+ipaPath)
+	ipaPath = os.path.expanduser(ipaPath)
+	ipaPath = unicode(ipaPath, "utf-8")
+	files = {'file': open(ipaPath, 'rb')}
+	headers = {'enctype':'multipart/form-data'}
+	payload = {'uKey':USER_KEY,'_api_key':API_KEY,'publishRange':'2','isPublishToPublic':'2', 'password':PYGER_PASSWORD}
+	print ("uploading....")
+	r = requests.post(PGYER_UPLOAD_URL, data = payload ,files=files,headers=headers)
+	if r.status_code == requests.codes.ok:
+		result = r.json()
+		parserPgyerUploadResult(result)
+	else:
+		print ('HTTPError,Code:'+r.status_code)
+
+def parserPgyerUploadResult(jsonResult):
+	resultCode = jsonResult['code']
+	if resultCode == 0:
+		downUrl = DOWNLOAD_BASE_URL +"/"+jsonResult['data']['appShortcutUrl']
+		print ("Upload Success\n" + "DownUrl is:" + downUrl)
+	else:
+		print ("Upload Fail! \n" + "Reason:"+jsonResult['message'])
+
+def upload2AppleStore():	
+	print ("~~~~~~~~~~~~~~~~是否上传到AppStore~~~~~~~~~~~~~~~~")
+	print ("        1 不上传 (默认)")
+	print ("        2 上传 ")
+	isuploadappstore = raw_input("您的决定：")
+	if isuploadappstore == '2':
+		uploadIpaToAppStore()
+
+def uploadIpaToAppStore():
+	print ("iPA上传中....")
+	altoolPath = "/Applications/Xcode.app/Contents/Applications/Application\ Loader.app/Contents/Frameworks/ITunesSoftwareService.framework/Versions/A/Support/altool"
+
+	exportCmd = "%s --validate-app -f %s -u %s -p %s -t ios --output-format xml" % (altoolPath, IPAPATH, APPLEID,APPLEPWD)
+	process = subprocess.Popen(exportCmd, shell=True)
+	(stdoutdata, stderrdata) = process.communicate()
+
+	validateResult = process.returncode
+	if validateResult == 0:
+		print ('~~~~~~~~~~~~~~~~iPA验证通过~~~~~~~~~~~~~~~~')
+		exportCmd = "%s --upload-app -f %s -u %s -p %s -t ios --output-format normal" % (
+		altoolPath, IPAPATH, APPLEID, APPLEPWD)
+		process = subprocess.Popen(exportCmd, shell=True)
+		(stdoutdata, stderrdata) = process.communicate()
+
+		uploadresult = process.returncode
+		if uploadresult == 0:
+			print ('~~~~~~~~~~~~~~~~iPA上传成功')
+		else:
+			print ('~~~~~~~~~~~~~~~~iPA上传失败')
+	else:
+		print ("~~~~~~~~~~~~~~~~iPA验证失败~~~~~~~~~~~~~~~~")
+		
+
+def cleanArchiveFile():
+	cleanCmd = "rm -r %s" %(ARCHIVEPATH)
+	process = subprocess.Popen(cleanCmd, shell = True)
+	process.wait()
+	print ("cleaned archiveFile: %s" %(ARCHIVEPATH))
+
+def Cleanup():
+	print ("~~~~~~~~~~~~~~~~是否删除archive文件~~~~~~~~~~~~~~~~")
+	print ("        1 保留 (默认)")
+	print ("        2 删除 ")
+	iscleararchive = raw_input("您的决定：")
+	if iscleararchive == "2":
+		cleanArchiveFile()
+
+def writeConfig2InfoPlist():
+	os.system('/usr/libexec/PlistBuddy -c "Set:CFBundleShortVersionString %s" %s' % (VERSION, PLIST_PATH))
+	global BUILD
+	if BUILD == '0':
+		cmd = '/usr/libexec/PlistBuddy -c "Print:CFBundleVersion" %s' % (PLIST_PATH)
+		buildNumber = int(os.popen(cmd).read().replace("\n", "")) + 1
+		BUILD = str(buildNumber)
+		print (BUILD)
+	else:
+		os.system('/usr/libexec/PlistBuddy -c "Set:CFBundleVersion %s" %s' % (BUILD, PLIST_PATH))
+
+
 def xcbuild(options):
+	print ('options:', options)
+
 	project = options.project
 	workspace = options.workspace
 
 	if project is None and workspace is None:
-		pass
-	elif project is not None:
+		print('project and workspace is None, exit.')
+		return
+	
+	printConfiguration()
+	writeConfig2InfoPlist()
+
+	if project is not None:
+		print('build project')
 		buildProject(project)
 	elif workspace is not None:
+		print('workspace')
 		buildWorkspace(workspace)
 
 	#导出ipa文件
-	exportarchive = exportArchive()
-	print "~~~~~~~~~~~~~~~~是否上传到蒲公英~~~~~~~~~~~~~~~~"
-	print "        1 不上传 (默认)"
-	print "        2 上传 "
-	isuploadpgyer = raw_input("您的决定：")
-	if isuploadpgyer == "2" and exportarchive != "":
-		uploadIpaToPgyer(IPAPATH)
+	exportArchive()
+	# exportarchive = exportArchive()
+	# upload2Pgyer()
+	# upload2AppleStore()
 
-	print "~~~~~~~~~~~~~~~~是否上传到AppStore~~~~~~~~~~~~~~~~"
-	print "        1 不上传 (默认)"
-	print "        2 上传 "
-	isuploadappstore = raw_input("您的决定：")
-	if isuploadappstore == '2':
-		uploadIpaToAppStore()
-	else:
-		print "~~~~~~~~~~~~~~~~是否删除archive文件~~~~~~~~~~~~~~~~"
-		print "        1 保留 (默认)"
-		print "        2 删除 "
-		iscleararchive = raw_input("您的决定：")
-		if iscleararchive == "2":
-			cleanArchiveFile()
+def checkBuildConfiguration():
+	print (sys._getframe().f_code.co_name)
+	if os.path.exists(PLIST_PATH) != True:
+		print ('plist not exists!')
+		return False
+	return True
 
 
 def main():
-
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-w", "--workspace", help="Build the workspace name.xcworkspace.", metavar="name.xcworkspace")
 	parser.add_argument("-p", "--project", help="Build the project name.xcodeproj.", metavar="name.xcodeproj")
-
 	options = parser.parse_args()
+	# xcbuild(options)
 
-	print "options: %s" % (options)
-
-	os.system('/usr/libexec/PlistBuddy -c "Set:CFBundleShortVersionString %s" %s' % (VERSION,PLIST_PATH))
-	os.system('/usr/libexec/PlistBuddy -c "Set:CFBundleVersion %s" %s' % (BUILD, PLIST_PATH))
-
-	xcbuild(options)
+	if checkBuildConfiguration():
+		return
+	# writeConfig2InfoPlist()
 
 if __name__ == '__main__':
 	main()
